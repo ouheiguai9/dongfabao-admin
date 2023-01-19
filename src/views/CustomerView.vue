5f60ac007cfb6db49d98248b456937b2ae77bb2c
田伯光 <349382440@qq.com>
2023-01-19 15:49:23 +0800

    客户管理修改

--- src/views/CustomerView.vue
+++ src/views/CustomerView.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>CustomerView</div>
+  <el-card shadow="never">
+    <el-table :data="[]" border style="width: 100%">
+      <el-table-column label="手机号" prop="date" width="180" />
+      <el-table-column label="用户姓名" prop="name" width="180" />
+      <el-table-column label="最后订单号" prop="address" />
+      <el-table-column label="订单金额" prop="address" />
+      <el-table-column label="订单时间" prop="address" />
+      <el-table-column label="操作" prop="address" />
+    </el-table>
+  </el-card>
 </template>
